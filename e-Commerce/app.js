55dc44517be65bfacf4e774b10b14b433d4b3568
Nikhil Gupta <nikhilg7404@gmail.com>
2024-04-05 15:55:14 +0530

    Serving files Statically

--- e-Commerce/app.js
+++ e-Commerce/app.js
@@ -1,13 +1,16 @@
 const http = require('http');
+const path = require("path");
 const adminRoute = require('./routes/admin')
 const shopRoute = require('./routes/shop')
 const bodyParser = require('body-parser');
 
 const express = require('express');
+const exp = require('constants');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use('/admin',adminRoute)
 app.use(shopRoute)
